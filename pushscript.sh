1f5158fe41e6884810732e87bbd5a4fad092c338
César González Segura <cegonse@posgrado.upv.es>
2016-03-15 13:21:33 +0100

    Final gitscripts

--- pushscript.sh
+++ pushscript.sh
@@ -4,14 +4,15 @@
 GITDIR="tu-path-al-git"
 
 # Carpeta del codigo en Unity
-UNITYDIR="tu-path-al-unityproject-assets-source"
+UNITYDIR="tu-path-al-unityproject-assets-source-client"
 
 # Mensaje de commit
 MSG=$1
 
-`cp -rf $UNITYDIR/ $GITDIR/client/`
-`cd $GITDIR`
-`find . -type f -name '*.meta' -delete`
-`git add .`
-`git commit -m $MSG`
-`git push origin master`
+cp -rf $UNITYDIR/ $GITDIR/
+cd $GITDIR
+find . -type f -name '*.meta' -delete
+rm -rf ./.vscode
+git add .
+git commit -m "$MSG"
+git push origin master
